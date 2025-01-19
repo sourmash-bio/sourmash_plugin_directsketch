65379876696c4d4c4defb8b3e8eeef569c705737
C. Titus Brown <titus@idyll.org>
2025-01-19 08:46:08 -0800

    WIP: ignore extra columns

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -123,11 +123,21 @@ pub fn load_gbassembly_info(input_csv: String) -> Result<(Vec<GBAssemblyData>, u
     // Check column names
     let header = rdr.headers()?;
     let expected_header = vec!["accession", "name"];
-    if header != expected_header {
-        return Err(anyhow!(
-            "Invalid column names in CSV file. Columns should be: {:?}",
-            expected_header
-        ));
+
+    for h in expected_header.iter() {
+        if !header.iter().any(|e| *h == e) {
+            return Err(anyhow!(
+                "Missing column name '{}' in CSV file. Columns should be: {:?}",
+                h,
+                expected_header
+            ));
+        }
+    }
+
+    for h in header.iter() {
+        if !expected_header.iter().any(|e| h == *e) {
+            eprintln!("WARNING: extra column '{}' in CSV file. Ignoring.", h);
+        }
     }
 
     for result in rdr.records() {
